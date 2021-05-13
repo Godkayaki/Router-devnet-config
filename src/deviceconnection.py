95ac6fe67194142042f70f02ba6e80615716c9e8
Godkayaki <daniplcm@gmail.com>
2021-05-13 18:22:27 +0200

    a

--- src/deviceconnection.py
+++ src/deviceconnection.py
@@ -6,9 +6,19 @@ import os
 from ncclient import manager
 from jinja2 import Template
 
-def test_connection(host, port, user, pswd):
+def test_connection(HOST, PORT, USER, PSWD):
     try:
         m = manager.connect(host=HOST, port=PORT, username=USER, password=PSWD, hostkey_verify=False, device_params={'name': 'csr'}, look_for_keys=False, allow_agent=False)
     except:
         return False
     return True
+
+def return_connection(HOST, PORT, USER, PSWD):
+    try:
+        m = manager.connect(host=HOST, port=PORT, username=USER, password=PSWD, hostkey_verify=False, device_params={'name': 'csr'}, look_for_keys=False, allow_agent=False)
+        return m
+    except:
+        return False
+
+#x = test_connection('ios-xe-mgmt.cisco.com', 10000, 'developer', 'C1sco12345')
+#print(x)
--- src/main.py
+++ src/main.py
@@ -3,6 +3,7 @@ import tkinter as tk
 import os
 import pygubu
 from tkinter import *
+from tkinter import messagebox
 
 import deviceconnection
 
@@ -19,9 +20,12 @@ class MainApp(Frame):
         
         self.defineWidgets()
         self.setupButtons()
+        self.disableChildren(self.config_frame)
 
     #define pygubu widgets
     def defineWidgets(self):
+        self.config_frame = self.builder.get_object('frame_configuration')
+        self.conn_frame = self.builder.get_object('frame_connection')
         self.entry_h = self.builder.get_object('entry_host')
         self.entry_p = self.builder.get_object('entry_port')
         self.entry_u = self.builder.get_object('entry_user')
@@ -31,23 +35,68 @@ class MainApp(Frame):
     def setupButtons(self):
         self.bt_testc = self.builder.get_object('button_testconnect')
         self.bt_c = self.builder.get_object('button_connect')
+        self.bt_disc = self.builder.get_object('button_disconnect')
 
         self.bt_testc.bind("<Button-1>", self.test_connection_clicked)
         self.bt_c.bind("<Button-1>", self.connection_clicked)
+        self.bt_disc.bind("<Button-1>", self.disconnet_from)
+
+    #disable all children from parent passed as reference
+    def disableChildren(self, parent):
+        for child in parent.winfo_children():
+            wtype = child.winfo_class()
+            if wtype not in ('Frame','Labelframe'):
+                child.configure(state='disable')
+            else:
+                self.disableChildren(child)
+
+    #enable all children from parent passed as reference
+    def enableChildren(self, parent):
+        for child in parent.winfo_children():
+            wtype = child.winfo_class()
+            if wtype not in ('Frame','Labelframe'):
+                child.configure(state='normal')
+            else:
+                self.enableChildren(child)
 
     #test connection button clicked event
     def test_connection_clicked(self, event):
         host = self.entry_h.get()
-        port = int(self.entry_p.get())
+        port = self.entry_p.get()
         user = self.entry_u.get()
         pswd = self.entry_pw.get()
         
-        if deviceconnection.test_connection(host, port, user, pswd):
-            messagebox.showinfo(message="Mensaje", title="Título")
+        if host != '' and port != '' and user != '' and pswd != '':
+            if not deviceconnection.test_connection(host, int(port), user, pswd):
+                messagebox.showinfo(message="No es pot establir una conexió amb el router.", title="Error de conexió.")
+            else:
+                messagebox.showinfo(message="La conexió amb el router s'ha prduït correctament.", title="Conexió correcte.")
+        else:
+            messagebox.showinfo(message="Els camps no poden estar buits.", title="Error de format.")
 
     #test connection button clicked event
     def connection_clicked(self, event):
-        pass
+        host = self.entry_h.get()
+        port = self.entry_p.get()
+        user = self.entry_u.get()
+        pswd = self.entry_pw.get()
+
+        if host != '' and port != '' and user != '' and pswd != '':
+            c = deviceconnection.return_connection(host, int(port), user, pswd)
+            if c == False:
+                messagebox.showinfo(message="No es pot establir una conexió amb el router.", title="Error de conexió.")
+            else:
+                print("conexio exitosa.")
+                self.disableChildren(self.conn_frame)
+                self.enableChildren(self.config_frame)
+
+        else:
+            messagebox.showinfo(message="Els camps no poden estar buits.", title="Error de format.")
+
+    #quit connection
+    def disconnet_from(self, event):
+        self.disableChildren(self.config_frame)
+        self.enableChildren(self.conn_frame)
 
     #run application
     def run(self):
