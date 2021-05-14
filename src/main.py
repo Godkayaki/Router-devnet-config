c82f35644132e9e687e76e8faa8ff18eeaecd50a
Godkayaki <daniplcm@gmail.com>
2021-05-14 19:12:26 +0200

    gui_adv

--- src/main.py
+++ src/main.py
@@ -6,13 +6,28 @@
 import tkinter as tk
 import os
 import pygubu
+import tkmacosx
 from tkinter import *
 from tkinter import messagebox
-
+from sys import platform
 import deviceconnection
+
+if platform == "linux" or platform == "linux2":
+    PLATFORM = "gnu"
+elif platform == "darwin":
+    PLATFORM = "mac"
+elif platform == "win32":
+    PLATFORM = "win"
+else:
+    sys.exit(0)
 
 PROJECT_PATH = os.path.dirname(os.path.dirname(__file__))
 GUI_FILE = PROJECT_PATH+"/gui/mainui.ui"
+
+if PLATFORM == "mac":
+    GUI_FILE = PROJECT_PATH+"/gui/mainui_mac.ui"
+else:
+    GUI_FILE = PROJECT_PATH+"/gui/mainui.ui"
 
 class MainApp(Frame):
     
@@ -30,6 +45,10 @@ class MainApp(Frame):
     def defineWidgets(self):
         self.config_frame = self.builder.get_object('frame_configuration')
         self.conn_frame = self.builder.get_object('frame_connection')
+        self.test_c_frame = self.builder.get_object('frame4')
+        self.c_frame = self.builder.get_object('frame5')
+        self.disc_frame = self.builder.get_object('frame_disconnect_bt')
+
         self.entry_h = self.builder.get_object('entry_host')
         self.entry_p = self.builder.get_object('entry_port')
         self.entry_u = self.builder.get_object('entry_user')
@@ -37,9 +56,18 @@ class MainApp(Frame):
 
     #setup buttons method
     def setupButtons(self):
-        self.bt_testc = self.builder.get_object('button_testconnect')
-        self.bt_c = self.builder.get_object('button_connect')
-        self.bt_disc = self.builder.get_object('button_disconnect')
+        if PLATFORM == "mac":
+            self.bt_testc = tkmacosx.Button(self.test_c_frame, text='Provar conexió', bg='white',fg='black', borderless=1)
+            self.bt_testc.pack(side="right")
+            self.bt_c = tkmacosx.Button(self.c_frame, text='Conectar', bg='white',fg='black', borderless=1)
+            self.bt_c.pack(side="left")
+            self.bt_disc = tkmacosx.Button(self.disc_frame, text='Desconectar', bg='white',fg='black', borderless=1)
+            self.bt_disc.pack(side="right", padx=30)
+        else:
+        
+            self.bt_testc = self.builder.get_object('button_testconnect')
+            self.bt_c = self.builder.get_object('button_connect')
+            self.bt_disc = self.builder.get_object('button_disconnect')
 
         self.bt_testc.bind("<Button-1>", self.test_connection_clicked)
         self.bt_c.bind("<Button-1>", self.connection_clicked)
@@ -50,7 +78,7 @@ class MainApp(Frame):
         for child in parent.winfo_children():
             wtype = child.winfo_class()
             if wtype not in ('Frame','Labelframe'):
-                child.configure(state='disable')
+                child.configure(state='disabled')
             else:
                 self.disableChildren(child)
 
@@ -74,7 +102,7 @@ class MainApp(Frame):
             if not deviceconnection.test_connection(host, int(port), user, pswd):
                 messagebox.showinfo(message="No es pot establir una conexió amb el router.", title="Error de conexió.")
             else:
-                messagebox.showinfo(message="La conexió amb el router s'ha prduït correctament.", title="Conexió correcte.")
+                messagebox.showinfo(message="La conexió amb el router s'ha produït correctament.", title="Conexió correcte.")
         else:
             messagebox.showinfo(message="Els camps no poden estar buits.", title="Error de format.")
 
@@ -90,7 +118,7 @@ class MainApp(Frame):
             if c == False:
                 messagebox.showinfo(message="No es pot establir una conexió amb el router.", title="Error de conexió.")
             else:
-                print("conexio exitosa.")
+                print("Conexió exitosa.")
                 self.disableChildren(self.conn_frame)
                 self.enableChildren(self.config_frame)
 
